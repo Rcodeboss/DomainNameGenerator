3712ee658376cf2f020ec87b50adab14986dc25d
Rcodeboss <morejonrafa@gmail.com>
2021-08-28 01:24:05 +0000

    Added DomainNameGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let string =
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)];
+
+  document.write(string + ".com");
+
   console.log("Hello Rigo from the console!");
 };
